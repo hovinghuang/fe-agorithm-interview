4242773abcd7bb3b6601ee73b3f21f8c7352febe
hovinghuang <hovinghuang@163.com>
2022-05-17 22:07:35 +0800

    update

--- src/00-codetop-fontend-100/28-max-area-of-island.ts
+++ src/00-codetop-fontend-100/28-max-area-of-island.ts
@@ -1,9 +1,9 @@
 // @ts-nocheck
 /**
- * 题目名称：
- * leetcode 题目: 
+ * 题目名称：岛屿的最大面积
+ * leetcode 题目: https://leetcode.cn/problems/max-area-of-island/
  * leetcode 题解: 
- * 牛客网 题目: 
+ * 牛客网 题目: https://www.nowcoder.com/practice/5568943d3a08403f932a5e54ec3ece71
  * 牛客网 题解: 
  */
 
